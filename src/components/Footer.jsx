381a8b05eb01e78fefcad4d6ef85212ecee93813
amruthesh <amrutheshamru16@gmail.com>
2024-03-19 19:01:44 +0530

    a

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer className="bg-[#1d9bf0] text-center dark:bg-neutral-700 lg:text-left">
+        <div className="bg-black/5 p-4 text-center text-surface text-white dark:text-white">
+          © 2024 Copyright : <a>Amruthesh Gowda</a>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
